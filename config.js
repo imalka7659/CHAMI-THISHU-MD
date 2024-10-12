f2ccc85b02ef41c2976611d368a9de9c95ac6ef4
imalka7659 <136040714+imalka7659@users.noreply.github.com>
2024-10-13 00:07:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "mVtWDRYK#L8pRdycx2i74SiwdG13F2mg-8NE9LF9yMJjMb56phgg",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://iili.io/dbFAKoG.jpg",
 
